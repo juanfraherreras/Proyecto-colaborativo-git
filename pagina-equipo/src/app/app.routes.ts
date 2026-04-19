0d80e49cc1fec436b8817bc2ecfeb906a31e25ca
juanfraherreras <juanfraherreras@gmail.com>
2026-04-19 14:10:58 -0500

    feat: Bazar Andrecito funcionando y reconstruido

--- pagina-equipo/src/app/app.routes.ts
+++ pagina-equipo/src/app/app.routes.ts
@@ -2,7 +2,7 @@ import { Routes } from '@angular/router';
 import { InicioComponent } from './inicio/inicio.component';
 
 export const routes: Routes = [
-  { path: '', component: InicioComponent }, // Página inicial
-  { path: 'conocenos', component: InicioComponent }, // Por ahora apuntan a inicio
-  { path: 'productos', component: InicioComponent }  // Hasta que los chicos suban lo suyo
+  { path: '', component: InicioComponent },
+  { path: 'conocenos', component: InicioComponent },
+  { path: 'productos', component: InicioComponent }
 ];
